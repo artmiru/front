062f2caf4664d1b1e935177b81a0bb3cde371a85
Evsei <artmiru@gmail.com>
2024-04-23 23:52:16 +0300

    header finish

--- resources/views/layouts/includes/header.blade.php
+++ resources/views/layouts/includes/header.blade.php
@@ -1,33 +1,43 @@
-<div class="container mx-auto pb-1 grid grid-cols-1 sm:grid-cols-4 gap-4 items-center">
-    <div class="justify-self-center">
-        <a class="flex items-end text-gray-800" href="/">
+<div class="md:container md:mx-auto py-1 grid grid-cols-1 sm:grid-cols-4 gap-4 items-center">
+    <div class="divide-y justify-self-center">
+        <a class="flex text-gray-800" href="/">
             <img class="mr-2" alt="Школа рисования для взрослых 'АртМир'" src="https://artmir.ru/img/logo.png"
                 style="height:60px;">
-            <div>
-                ARTMIR.RU
-                <div class="text-lg">студия рисования</div>
-            </div>
+            <span class="flex flex-col justify-end pb-1">
+                <span class="text-3xl  leading-none">ARTMIR.RU</span>
+                <span class="text-lg  leading-none">студия рисования</span>
+            </span>
         </a>
         <div>1 минута от м. Звенигородская</div>
     </div>
 
-    <div class="sm:col-span-2 text-2xl sm:text-3xl">
+    <div class="sm:col-span-2 text-2xl sm:text-2xl leading-7 sm:leading-8 sm:self-center">
         Обучение рисованию и живописи взрослых <span class="whitespace-nowrap">в Санкт-Петербурге</span>
     </div>
 
-    <div class="flex flex-col items-center justify-center space-y-2">
+    <div class="flex flex-col items-center justify-center space-y-4 sm:space-y-1 py-1">
         <div class="flex">
-            <a class="text-white bg-blue-500 py-2 px-4 rounded-l" href="https://t.me/artmir_zven" target="_blank"
-                rel="noopener noreferrer">
-                <!-- SVG for Telegram -->
+            <a class="text-white bg-blue-400 hover:bg-blue-500 py-1 sm:py-0 px-4 rounded-l flex items-center"
+                href="https://t.me/artmir_zven" target="_blank" rel="noopener noreferrer">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                    class="bi bi-telegram me-1 my-2" viewBox="0 0 16 16">
+                    <path
+                        d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0M8.287 5.906q-1.168.486-4.666 2.01-.567.225-.595.442c-.03.243.275.339.69.47l.175.055c.408.133.958.288 1.243.294q.39.01.868-.32 3.269-2.206 3.374-2.23c.05-.012.12-.026.166.016s.042.12.037.141c-.03.129-1.227 1.241-1.846 1.817-.193.18-.33.307-.358.336a8 8 0 0 1-.188.186c-.38.366-.664.64.015 1.088.327.216.589.393.85.571.284.194.568.387.936.629q.14.092.27.187c.331.236.63.448.997.414.214-.02.435-.22.547-.82.265-1.417.786-4.486.906-5.751a1.4 1.4 0 0 0-.013-.315.34.34 0 0 0-.114-.217.53.53 0 0 0-.31-.093c-.3.005-.763.166-2.984 1.09">
+                    </path>
+                </svg>
                 Telegram
             </a>
-            <a class="text-white bg-green-500 py-2 px-4 -ml-1 rounded-r" href="https://wa.me/+79219076449"
-                target="_blank" rel="noopener noreferrer">
-                <!-- SVG for WhatsApp -->
+            <a class="text-white bg-green-400 hover:bg-green-500 py-1 sm:py-0 px-4 -ml-1 rounded-r flex items-center"
+                href="https://wa.me/+79219076449" target="_blank" rel="noopener noreferrer">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                    class="bi bi-whatsapp me-1 my-2" viewBox="0 0 16 16">
+                    <path
+                        d="M13.601 2.326A7.85 7.85 0 0 0 7.994 0C3.627 0 .068 3.558.064 7.926c0 1.399.366 2.76 1.057 3.965L0 16l4.204-1.102a7.9 7.9 0 0 0 3.79.965h.004c4.368 0 7.926-3.558 7.93-7.93A7.9 7.9 0 0 0 13.6 2.326zM7.994 14.521a6.6 6.6 0 0 1-3.356-.92l-.24-.144-2.494.654.666-2.433-.156-.251a6.56 6.56 0 0 1-1.007-3.505c0-3.626 2.957-6.584 6.591-6.584a6.56 6.56 0 0 1 4.66 1.931 6.56 6.56 0 0 1 1.928 4.66c-.004 3.639-2.961 6.592-6.592 6.592m3.615-4.934c-.197-.099-1.17-.578-1.353-.646-.182-.065-.315-.099-.445.099-.133.197-.513.646-.627.775-.114.133-.232.148-.43.05-.197-.1-.836-.308-1.592-.985-.59-.525-.985-1.175-1.103-1.372-.114-.198-.011-.304.088-.403.087-.088.197-.232.296-.346.1-.114.133-.198.198-.33.065-.134.034-.248-.015-.347-.05-.099-.445-1.076-.612-1.47-.16-.389-.323-.335-.445-.34-.114-.007-.247-.007-.38-.007a.73.73 0 0 0-.529.247c-.182.198-.691.677-.691 1.654s.71 1.916.81 2.049c.098.133 1.394 2.132 3.383 2.992.47.205.84.326 1.129.418.475.152.904.129 1.246.08.38-.058 1.171-.48 1.338-.943.164-.464.164-.86.114-.943-.049-.084-.182-.133-.38-.232">
+                    </path>
+                </svg>
                 WhatsApp
             </a>
         </div>
-        <a class="text-3xl" href="tel:+79219076449">907-64-49</a>
+        <a class="text-3xl pb-4 sm:pb-0" href="tel:+79219076449">907-64-49</a>
     </div>
 </div>
