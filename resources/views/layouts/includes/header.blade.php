f4cff7f6455c0b193f13dd92115effe5eff3ba0a
Evsei <artmiru@gmail.com>
2024-04-23 13:08:36 +0300

    header

---
+++ resources/views/layouts/includes/header.blade.php
@@ -0,0 +1,14 @@
+<div class="container columns-3 gap-1">
+    <div class="flex items-center col-start-2">
+        <div>
+            <img class="block" src="https://artmir.ru/img/logo.png" alt="АртМир студия рисования">
+            <div class="justify-center">
+                <div>ARTMIR.RU</div>
+                <div>студия рисования</div>
+            </div>
+        </div>
+        <div>1 минута от метро</div>
+    </div>
+    <div class="col-span-2">@yield('header', 'Обучение рисованию и живописи взрослых в Санкт-Петербурге')</div>
+    <div>rr</div>
+</div>
--- resources/views/layouts/temp.blade.php
+++ resources/views/layouts/temp.blade.php
@@ -1,15 +1,17 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title')</title>
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
+
 <body>
-    <div class="container">
-        sdsdsphp
-    </div>
- @yield('content')
+    <header>@include('layouts.includes.header')</header>
+    <main>@yield('content')</main>
+    <footer>@include('layouts.includes.footer')</footer>
 </body>
+
 </html>
