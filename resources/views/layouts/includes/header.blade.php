0f65c423b4529fb1e8d4c33ebc9739367dedef56
Evsei <artmiru@gmail.com>
2024-04-23 16:20:16 +0300

    header2

--- resources/views/layouts/includes/header.blade.php
+++ resources/views/layouts/includes/header.blade.php
@@ -1,14 +1,33 @@
-<div class="container columns-3 gap-1">
-    <div class="flex items-center col-start-2">
-        <div>
-            <img class="block" src="https://artmir.ru/img/logo.png" alt="АртМир студия рисования">
-            <div class="justify-center">
-                <div>ARTMIR.RU</div>
-                <div>студия рисования</div>
+<div class="container mx-auto pb-1 grid grid-cols-1 sm:grid-cols-4 gap-4 items-center">
+    <div class="justify-self-center">
+        <a class="flex items-end text-gray-800" href="/">
+            <img class="mr-2" alt="Школа рисования для взрослых 'АртМир'" src="https://artmir.ru/img/logo.png"
+                style="height:60px;">
+            <div>
+                ARTMIR.RU
+                <div class="text-lg">студия рисования</div>
             </div>
-        </div>
-        <div>1 минута от метро</div>
+        </a>
+        <div>1 минута от м. Звенигородская</div>
     </div>
-    <div class="col-span-2">@yield('header', 'Обучение рисованию и живописи взрослых в Санкт-Петербурге')</div>
-    <div>rr</div>
+
+    <div class="sm:col-span-2 text-2xl sm:text-3xl">
+        Обучение рисованию и живописи взрослых <span class="whitespace-nowrap">в Санкт-Петербурге</span>
+    </div>
+
+    <div class="flex flex-col items-center justify-center space-y-2">
+        <div class="flex">
+            <a class="text-white bg-blue-500 py-2 px-4 rounded-l" href="https://t.me/artmir_zven" target="_blank"
+                rel="noopener noreferrer">
+                <!-- SVG for Telegram -->
+                Telegram
+            </a>
+            <a class="text-white bg-green-500 py-2 px-4 -ml-1 rounded-r" href="https://wa.me/+79219076449"
+                target="_blank" rel="noopener noreferrer">
+                <!-- SVG for WhatsApp -->
+                WhatsApp
+            </a>
+        </div>
+        <a class="text-3xl" href="tel:+79219076449">907-64-49</a>
+    </div>
 </div>
--- resources/views/layouts/temp.blade.php
+++ resources/views/layouts/temp.blade.php
@@ -5,11 +5,12 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title')</title>
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    {{-- @vite(['resources/css/app.css', 'resources/js/app.js']) --}}
+     <script src="https://cdn.tailwindcss.com?plugins=forms,typography,aspect-ratio,line-clamp"></script>
 </head>
 
 <body>
-    <header>@include('layouts.includes.header')</header>
+    <header  class="text-center bg-white shadow">@include('layouts.includes.header')</header>
     <main>@yield('content')</main>
     <footer>@include('layouts.includes.footer')</footer>
 </body>
