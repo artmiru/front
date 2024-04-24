bc60468a416da69f7dc69980840d1e6049f5f4e7
Evsei <artmiru@gmail.com>
2024-04-24 18:07:57 +0300

    MAIN courses

--- resources/views/pages/main.blade.php
+++ resources/views/pages/main.blade.php
@@ -3,20 +3,21 @@
 @section('title', 'Home Page')
 
 @section('content')
-
-    <div class="bg-stone-100 py-8 text-center">
-        <div class="sm:container sm:mx-auto">
+<div class="shadow">
+    <div class="bg-stone-50 py-8 text-center">
+        <div class="sm:container sm:mx-auto sm:w-10/12">
             <h1 class="text-2xl font-normal mb-4 sm:text-6xl sm:mb-0">ХУДОЖЕСТВЕННЫЕ КУРСЫ</h1>
             <h2 class="sm:mb-8 sm:mt-2 sm:text-2xl sm:text-gray-400">ВЫ МОЖЕТЕ ЗАПИСАТЬСЯ НА ЛЮБОЙ ИЗ ЭТИХ КУРСОВ</h2>
             <div class="grid sm:grid-cols-3 gap-4 sm:gap-2">
                 @foreach ($artCourses as $course)
-                    <div class="bg-white shadow-md">
-                        <h3 class="text-xl my-1 ">{{ $course['title'] }}</h3>
-                        <img class="h-auto sm:max-w-full sm:w-auto w-max max-w-full mx-auto border mb-2"
+                    <div class="bg-white shadow-lg border rounded">
+                        <h3 class="text-2xl my-3 font-semibold">{{ $course['title'] }}</h3>
+                        <img class="h-auto w-full mx-auto border"
                             src="{{ $course['imageUrl'] }}" alt="{{ $course['title'] }}">
                     </div>
                 @endforeach
             </div>
         </div>
+    </div>
     </div>
 @endsection
