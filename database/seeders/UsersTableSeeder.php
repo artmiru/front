46708c736bceadd40fd120ab2f6c19ef34fbb190
Evsei <artmiru@gmail.com>
2024-04-29 23:18:47 +0300

    seederUser

---
+++ database/seeders/UsersTableSeeder.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\User;
+
+class UsersTableSeeder extends Seeder
+{
+    public function run(): void
+    {
+
+        $password = bcrypt('12345678'); // Hash the password once since it's the same for both users
+
+        User::insert([
+            [
+                'name' => 'Евсей',
+                'middle_name' => 'Олегович',
+                'last_name' => 'Ярошенко',
+                'phone' => '9219245228',
+                'email' => 'artmiru@gmail.com',
+                'password' => $password, // Use the hashed password
+            ],
+            [
+                'name' => 'Константин',
+                'middle_name' => 'Константинович',
+                'last_name' => 'Константинов',
+                'phone' => '9215778927',
+                'email' => 'kostyakk@mail.ru',
+                'password' => $password, // Use the same hashed password
+            ],[
+                'name' => 'Екатерина',
+                'last_name' => 'Муравина',
+                'phone' => '9818887546',
+                'password' => $password, // Use the same hashed password
+            ],[
+                'name' => 'Алексей',
+                'middle_name' => 'Викторович',
+                'last_name' => 'Добровольский',
+                'phone' => '9062595116',
+                'password' => $password, // Use the same hashed password
+            ],[
+                'name' => 'Илья',
+                'last_name' => 'Злобин',
+                'phone' => '9533444990',
+                'password' => $password, // Use the same hashed password
+            ],[
+                'name' => 'Александра',
+                'middle_name' => 'Вильевна',
+                'last_name' => 'Зиннурова',
+                'phone' => '9062283179',
+                'password' => $password, // Use the same hashed password
+            ],[
+                'name' => 'Анастасия',
+                'last_name' => 'Кокорева',
+                'phone' => '9050587690',
+                'password' => $password, // Use the same hashed password
+            ],[
+                'name' => 'Данил',
+                'last_name' => 'Андреев',
+                'phone' => '9045172507',
+                'password' => $password, // Use the same hashed password
+            ],
+        ]);
+    }
+}
