68579fab3c0d813f35695a286b9680e64c1f3eba
Evsei <artmiru@gmail.com>
2024-10-22 16:22:30 +0300

    user_subscriptions_table

---
+++ database/migrations/2024_10_22_131810_create_user_subscriptions_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_subscriptions', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->foreignId('subscription_id')->constrained()->onDelete('cascade');
+            $table->integer('minutes_used')->default(0);
+            $table->boolean('active')->default(true);
+            $table->timestamp('started_at');
+            $table->timestamp('expires_at');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_subscriptions');
+    }
+};
+
